ce25640875b09580eab0a42aa5edfcb44ab90ef0
Nikos Roussos <nikos@roussos.cc>
2019-03-28 15:32:35 +0200

    First iteration of GeoIP detection
    * Render country list from json file

--- static/js/main.js
+++ static/js/main.js
@@ -10,11 +10,14 @@ $(document).ready(function() {
   });
 
   // Language selection button dropdown
-  $('.dropdown-toggle').click(function() {
-    if($('.dropdown').hasClass('open')) {
+  let dropdown = false;
+  $(document).on('click', '.dropdown-toggle', function() {
+    if (dropdown) {
       $('.dropdown').removeClass('open');
+      dropdown = false;
     } else {
       $('.dropdown').addClass('open');
+      dropdown = true;
     }
     return false;
   });
@@ -22,4 +25,64 @@ $(document).ready(function() {
     $('.dropdown').removeClass('open');
   });
 
+  // Render countries list
+  $.ajax('/static/countries.json')
+    .then(
+      function success(response) {
+        let international_html = ``;
+        let sublist_html = ``;
+        $.each(response, function (letter, countries) {
+          if ( '0' === letter ) {
+            international_html += `<a class="international" href="${countries[0].url}">${countries[0].name}</a>`;
+          } else {
+            sublist_html += `<li><h3 class="country-group-letter">${letter}</h3>
+              <ul class="countries_sublist">`;
+            $.each(countries, function (index, country) {
+              $.each(country.lang, function(key, lang) {
+                sublist_html += `<li>
+                  <a href="${lang.url}">${country.name} | ${lang.name}</a>
+                  </li>`;
+              });
+            });
+            sublist_html += `</ul></li>`;
+          }
+        });
+        let countries_html = `<h2>All countries</h2>${international_html}<ul class="countries_list">${sublist_html}</ul>`;
+        $('#country-list').html(countries_html);
+      }
+    );
+
+  // IP Geolocation
+  function ipLookUp () {
+    $.ajax('http://ip-api.com/json')
+      .then(
+        function success(response) {
+          const countryCode = response.countryCode;
+          $.ajax('/static/countries.json')
+            .then(
+              function success(response) {
+                $.each(response, function (letter, countries) {
+                  $.each(countries, function (index, country) {
+                    if (countryCode == country.code) {
+                      $('#country_name').html(country.name);
+                      if (country.lang.length == 1) {
+                        $('#selection').attr('href', country.lang[0].url);
+                      } else {
+                        $('#selection .arrow').css('display', 'inline-block');
+                        $('#selection').addClass('dropdown-toggle');
+                        let items = ``;
+                        $.each(country.lang, function(key, lang) {
+                          items += `<a class="btn btn-secondary dropdown-item" href="${lang.url}">${lang.name}</a>`;
+                          $('.dropdown-group').html(items);
+                        });
+                      }
+                    }
+                  });
+                });
+              }
+            );
+        }
+      );
+  }
+  ipLookUp();
 });
