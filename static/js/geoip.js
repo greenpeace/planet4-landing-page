3ea1dcfec007601fe406ceccf2c2707639cf086b
Nikos Roussos <nikos@roussos.cc>
2019-06-27 15:40:43 +0300

    Add support of multiple country codes

--- static/js/geoip.js
+++ static/js/geoip.js
@@ -5,11 +5,12 @@ $(document).ready(function() {
 
   // IP Geolocation
   const countryCode = $('body').data('code');
+
   $.ajax(api).then(
     function success(response) {
       $.each(response, function (letter, countries) {
         $.each(countries, function (index, country) {
-          if (countryCode == country.code) {
+          if (country.codes && country.codes.includes(countryCode)) {
             $('#country_name').html(country.name);
             if (country.lang.length == 1) {
               $('#selection').attr('href', country.lang[0].url);
