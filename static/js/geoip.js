7cbd22c21f0d3ff85b41db48bdcbc6a6a568da61
Nikos Roussos <nikos.roussos@greenpeace.org>
2025-02-18 18:24:11 +0200

    Fix button link for NROs with more than one language

--- static/js/geoip.js
+++ static/js/geoip.js
@@ -8,11 +8,7 @@ Object.entries(data).forEach(letter => {
   letter[1].forEach(country => {
     if (country.codes && country.codes.includes(countryCode)) {
       byID('country-name').innerHTML = country.name;
-      if (country.lang.length == 1) {
-        byID('selection').setAttribute('href', country.lang[0].url);
-      } else {
-        byID('selection').setAttribute('href', '#');
-      }
+      byID('selection').setAttribute('href', country.lang[0].url);
     }
   });
 });
