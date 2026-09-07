9b1f69523a6d643709ad62d2f208638a426d17cb
Nikos Roussos <nikos.roussos@greenpeace.org>
2026-09-07 10:32:52 +0300

    Simplify GeoIP template and update

--- bin/geoipupdate.sh
+++ bin/geoipupdate.sh
@@ -1,23 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [[ -z "${GEOIP_ACCOUNTID}" ]]; then
-  echo "GEOIP_ACCOUNTID is blank, account id is required."
-  exit 1
-fi
-
-if [[ -z "${GEOIP_LICENSE}" ]]; then
-  echo "GEOIP_LICENSE is blank, license is required"
-  exit 1
-fi
-
 GEOIP_STATELESS_BUCKET="planet4-assets"
 GEOIP_FOLDER="/usr/share/GeoIP"
-GEOP_CONFIG="/etc/GeoIP.conf"
-
-dockerize -template "$GEOIP_CONFIG.tmpl:$GEOIP_CONFIG"
 
 # Update GeoIP data and upload them
-/usr/bin/geoipupdate -v
+sudo /usr/bin/geoipupdate -v
 mkdir -p $GEOIP_FOLDER
-gcloud storage rsync ${GEOIP_FOLDER} gs://"${GEOIP_STATELESS_BUCKET}/GeoIP/" --recursive
+if [[ -f "${GEOIP_FOLDER}/GeoLite2-Country.mmdb" && -f "${GEOIP_FOLDER}/GeoLite2-City.mmdb" ]]; then
+  gcloud storage rsync ${GEOIP_FOLDER} gs://"${GEOIP_STATELESS_BUCKET}/GeoIP" --recursive
+fi
