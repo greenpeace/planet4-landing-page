7e7d4acab4fc54ce34ecdc72de3702707ad3c5df
Nikos Roussos <nikos.roussos@greenpeace.org>
2026-09-10 11:29:25 +0300

    Exclude lock file from syncing

--- bin/geoipupdate.sh
+++ bin/geoipupdate.sh
@@ -5,8 +5,8 @@ GEOIP_STATELESS_BUCKET="planet4-assets"
 GEOIP_FOLDER="/usr/share/GeoIP"
 
 # Update GeoIP data and upload them
-mkdir -p $GEOIP_FOLDER
-/usr/bin/geoipupdate -v
+sudo mkdir -p $GEOIP_FOLDER
+sudo /usr/bin/geoipupdate -v
 if [[ -f "${GEOIP_FOLDER}/GeoLite2-Country.mmdb" && -f "${GEOIP_FOLDER}/GeoLite2-City.mmdb" ]]; then
-  gcloud storage rsync ${GEOIP_FOLDER} gs://"${GEOIP_STATELESS_BUCKET}/GeoIP" --recursive
+  gcloud storage rsync ${GEOIP_FOLDER} gs://"${GEOIP_STATELESS_BUCKET}/GeoIP" --recursive --exclude=".geoipupdate.lock"
 fi
